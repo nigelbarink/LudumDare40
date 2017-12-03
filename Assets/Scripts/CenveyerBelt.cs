f26568d405f6e41ff7207bff90206c700639cd81
nigel <nigelbarink@hotmail.com>
2017-12-03 10:50:24 +0100

    Initial commit

---
+++ Assets/Scripts/CenveyerBelt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CenveyerBelt : MonoBehaviour
+{
+    public float ConveyerSpeed = 5f;
+    
+	void Update ()
+	{
+	    foreach ( Transform belt in transform )
+	    {
+	        Vector3 position = belt.transform.position;
+	        position.x += ConveyerSpeed * Time.deltaTime;
+	        position.x = position.x >= 11f ? -11 : position.x;
+	        belt.transform.position = position;
+
+        }
+
+    }
+}
